9db4d89d43dc72ef0e575154aa20343159c8d5bd
Coderdivyansh6 <divyanshgoswami62@gmail.com>
2024-10-01 10:11:27 +0530

    Add files via upload

---
+++ smallestof3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+// Declaring the variable
+    int a,b,c;
+
+// taking user input
+    printf("enter your 3 numbers");
+    scanf("%d%d%d",&a,&b,&c);
+
+// using conditions 
+if( a < b && a < c){
+    printf("The smallest number is %d",a);
+}
+
+else if( b < a && b < c){
+    printf("The smallest number is %d",b);
+}
+
+else{
+    printf("The smallest number is %d",c);
+}  
+
+    return 0;
+
+}
